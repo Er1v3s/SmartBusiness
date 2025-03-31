762e60637a929b679d7a9007a56d431b64010314
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-31 14:20:45 +0200

    Add swagger services

--- SmartBusiness.Api/Program.cs
+++ SmartBusiness.Api/Program.cs
@@ -12,6 +12,8 @@ namespace SmartBusiness.Api
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
