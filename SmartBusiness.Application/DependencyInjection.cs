c7532d5d96823d2eebcc084e7aa50d5804cb8de7
Filip Statkiewicz <stf6.dev@gmail.com>
2025-03-31 17:16:51 +0200

    Create register user endpoint, command and command handler

---
+++ SmartBusiness.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using SmartBusiness.Domain.Entities;
+
+namespace SmartBusiness.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                //cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
+
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
+            return services;
+        }
+    }
+}
