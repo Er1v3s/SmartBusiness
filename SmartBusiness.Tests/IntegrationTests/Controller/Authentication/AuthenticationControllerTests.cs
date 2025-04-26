41592388c08f38249259daee8769b57f898d7f65
Filip Statkiewicz <stf6.dev@gmail.com>
2025-04-26 16:54:09 +0200

    test: Create integration tests of an exception scenario during authentication

--- SmartBusiness.Tests/IntegrationTests/Controller/Authentication/AuthenticationControllerTests.cs
+++ SmartBusiness.Tests/IntegrationTests/Controller/Authentication/AuthenticationControllerTests.cs
@@ -33,5 +33,43 @@ namespace SmartBusiness.Tests.IntegrationTests.Controller.Authentication
             // Assert
             result.StatusCode.Should().Be(HttpStatusCode.OK);
         }
+        
+        [Fact]
+        public async Task Login_WhenUserDoesNotExist_ReturnsNotFoundResult()
+        {
+            // Arrange
+            await _integrationTestsHelper.EnsureThereIsNoDataInDb();
+            var user = new User
+            {
+                Username = "johndoe",
+                Email = "johndoe@gmail.com",
+                PasswordHash = "!Qwerty123",
+            };
+
+            var request = new LoginRequest(user.Email, user.PasswordHash); // Password is not hashed yet.
+            var content = JsonContent.Create(request); 
+            
+            // Act 
+            var result = await _client.PostAsync($"/api/user/authentication", content);
+            
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+        
+        [Fact]
+        public async Task Login_WhenInvalidPassword_ReturnsBadRequestResult()
+        {
+            // Arrange
+            var user = await _integrationTestsHelper.SeedDatabaseAndGenerateUserAsync();
+
+            var request = new LoginRequest(user.Email, $"{user.PasswordHash} invalidPassword"); // Password is not hashed yet.
+            var content = JsonContent.Create(request); 
+            
+            // Act 
+            var result = await _client.PostAsync($"/api/user/authentication", content);
+            
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
     }
 }
