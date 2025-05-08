99b9fc4d7c302fef006ff326658b2ff6f8f07db2
Filip Statkiewicz <stf6.dev@gmail.com>
2025-05-08 16:08:41 +0200

    feat: Create Sales API microservice

---
+++ src/Services/SalesService/SalesService.Api/Controllers/StatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesService.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetServiceStatus() => Ok(new { status = "Sales Service is working correctly! \ud83d\udfe2" });
+    }
+}
---
+++ src/Services/SalesService/SalesService.Infrastructure/SalesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SalesService.Infrastructure
+{
+    public class SalesDbContext : DbContext
+    {
+        public SalesDbContext(DbContextOptions<SalesDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
