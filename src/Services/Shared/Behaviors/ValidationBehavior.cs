34f97d5361c985fa6a9412844688c0b70c20cc61
Filip Statkiewicz <stf6.dev@gmail.com>
2025-05-10 13:48:40 +0200

    Change files structure

---
+++ src/Services/Shared/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using FluentValidation;
+using Shared.Exceptions;
+
+namespace Shared.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResult = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+            var failures = validationResult
+                .SelectMany(v => v.Errors)
+                .Select(x => new ValidationError
+                {
+                    Property = x.PropertyName,
+                    ErrorMessage = x.ErrorMessage
+                }).ToList();
+
+            if (failures.Any())
+                throw new CustomValidationException(failures);
+
+            var response = await next(cancellationToken);
+
+            return response;
+        }
+    }
+}
---
+++ src/Services/Shared/Exceptions/CustomValidationException.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Exceptions
+{
+    public class CustomValidationException : Exception
+    {
+        public List<ValidationError> ValidationErrors { get; set; }
+
+        public CustomValidationException(List<ValidationError> validationErrors)
+        {
+            ValidationErrors = validationErrors;
+        }
+    }
+}
