9d910b5119bd0da6fa51cdc4dd0363698daa291d
Filip Statkiewicz <stf6.dev@gmail.com>
2025-05-12 14:45:37 +0200

    feat: Create Company, Roles and linking table entities, deleted unused

---
+++ src/Services/AuthService/AuthService.Domain/Entities/Company.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NanoidDotNet;
+
+namespace AuthService.Domain.Entities
+{
+    public class Company
+    {
+        [Key]
+        public string Id { get; set; } = Nanoid.Generate(size: 17);
+        public string Name { get; set; } = string.Empty;
+        public int? CreatedByUserId { get; set; }
+        public virtual User? CreatedByUser { get; set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+
+        public ICollection<UserCompanyRole> UserCompanyRoles { get; set; } = new List<UserCompanyRole>();
+    }
+}
